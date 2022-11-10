421edbb26f6d5697ce8b3d5d257b3c48a1c9cf3c
Nekrut1 <vladnekrutenko1@gmail.com>
2022-11-10 21:27:00 +0200

    LJ3-137(HW6)

--- src/main/java/com/nekrutenko/Main.java
+++ src/main/java/com/nekrutenko/Main.java
@@ -1,33 +1,16 @@
 package com.nekrutenko;
 
+import com.nekrutenko.model.Car;
+import com.nekrutenko.service.CarService;
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Vladyslav Nekrutenko";
-        System.out.println(name);
+        Car firstCar = CarService.create();
+        Car secondCar = CarService.create();
+        Car thirdCar = CarService.create();
 
-        System.out.println("-------------------");
-
-        int i = 0;
-        int y = 5;
-        do { System.out.println(i + " Step," + "value " + y);
-            i++;
-            y+=2;
-        }
-        while (i <= 10);
-
-        System.out.println("-------------------");
-
-            for (int j = 0; j <= 10; j++) {
-                if (j == 3) {
-                    continue;
-                }
-                if (j == 6) {
-                    break;
-                }
-                System.out.println(j + " Step");
-
-            }
-
-        }
+        CarService.print(firstCar);
+        CarService.print(secondCar);
+        CarService.print(thirdCar);
     }
-
+}
